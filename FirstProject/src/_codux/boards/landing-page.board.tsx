a704f9876eba8067c6d04ffee872f6971da1c997
Qadira Moore (RIT Student) <qm6148@rit.edu>
2024-09-13 18:49:04 -0400

    create portfolio

---
+++ FirstProject/src/_codux/boards/landing-page.board.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { createBoard } from "@wixc3/react-board";
+
+export default createBoard({
+  name: "Landing Page",
+  Board: () => (
+    <div>
+      <div></div>
+    </div>
+  ),
+  isSnippet: true,
+});
